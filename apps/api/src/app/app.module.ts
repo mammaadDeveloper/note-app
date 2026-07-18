023d33629d0f58ab0c9c7cc52ba4e175afcad5ac
Mohammadreza <developerMammad@proton.me>
2026-07-18 21:32:31 +0330

    feat(api): api setup
    Initial setup of the API project; creating the necessary configurations, including database
    configurations.

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: '../../../../.env',
+    load: []
+  }),
+  TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: (config: ConfigService) => ({
+      type: 'postgres',
+      url: config.get('database.url'),
+      autoLoadEntities: true,
+      synchronize: true
+    })
+  })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
