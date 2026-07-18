1c8c090bcd3dff04ee7bb359c2f2b834d8219760
Mohammadreza <developerMammad@proton.me>
2026-07-18 23:03:35 +0330

    feat(api): backend configurations and initial setup

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,18 @@
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  console.log(process.env.DATABASE_URL);
+
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    forbidNonWhitelisted: true,
+    forbidUnknownValues: true,
+  }))
   const port = process.env.PORT || 3000;
   await app.listen(port);
 }
